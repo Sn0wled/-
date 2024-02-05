05d023626dc2ff2e17985b487c0ff9793d171be9
Sn0wled <misha.safin.sn0wled@gmail.com>
2024-02-05 10:44:47 +0500

    asd

--- grom/grom/10-faus.cpp
+++ grom/grom/10-faus.cpp
@@ -8,11 +8,105 @@
 
 // ознакомительные действия с автоматом
 void fa_general(NFA & fa) {
+    SYMB A = fa.reg_state_id("A");
+    SYMB B = fa.reg_state_id("B");
+    SYMB C = fa.reg_state_id("C");
+    SYMB D = fa.reg_state_id("D");
+    SYMB E = fa.reg_state_id("E");
+    SYMB F = fa.reg_state_id("F");
+    SYMB G = fa.reg_state_id("G");
+    TRAN tran;
+    tran.a = A;
+    tran.b = B;
+    tran.c = '0';
+    fa.tran_add(tran);
+
+    tran.a = A;
+    tran.b = B;
+    tran.c = '0';
+    fa.tran_add(tran);
+
+    tran.a = A;
+    tran.b = C;
+    tran.c = '1';
+    fa.tran_add(tran);
+
+    tran.a = B;
+    tran.b = D;
+    tran.c = '1';
+    fa.tran_add(tran);
+
+    tran.a = C;
+    tran.b = E;
+    tran.c = '1';
+    fa.tran_add(tran);
+
+    tran.a = D;
+    tran.b = C;
+    tran.c = '0';
+    fa.tran_add(tran);
+
+    tran.a = D;
+    tran.b = E;
+    tran.c = '1';
+    fa.tran_add(tran);
+
+    tran.a = E;
+    tran.b = B;
+    tran.c = '0';
+    fa.tran_add(tran);
+
+    tran.a = E;
+    tran.b = D;
+    tran.c = '1';
+    fa.tran_add(tran);
+
+    tran.a = F;
+    tran.b = D;
+    tran.c = '0';
+    fa.tran_add(tran);
+
+    tran.a = G;
+    tran.b = F;
+    tran.c = '0';
+    fa.tran_add(tran);
+
+    tran.a = G;
+    tran.b = E;
+    tran.c = '1';
+    fa.tran_add(tran);
+
+    tran.a = G;
+    tran.b = F;
+    tran.c = '1';
+    fa.tran_add(tran);
+
+    fa.initials.insert(A);
+    fa.finals.insert(D);
+    fa.finals.insert(E);
 }
 
 // устранение недостижимых состояний конечного автомата
 void fa_remove_unreachable(NFA & fa1, NFA & fa2) {
-    fa_general(fa2);
+    //fa_general(fa2);
+    SSET r = fa1.initials;
+    for (int i = 1; i <= r.count(); i++) {
+        fa2.reg_initial_from(fa1, r[i]);
+    }
+    while (true)
+    {
+        int startCount = r.count();
+        for (int i = 1; i <= fa1.delta_count(); i++) {
+            if (r.contains(fa1[i].a)) r.insert(fa1[i].b);
+        }
+        if (startCount == r.count()) break;
+    }
+    for (int i = 1; i <= fa1.finals.count(); i++) {
+        if (r.contains(fa1.finals[i])) fa2.reg_final_from(fa1, fa1.finals[i]);
+    }
+    for (int i = 1; i <= fa1.delta_count(); i++) {
+        if (r.contains(fa1[i].b) && r.contains(fa1[i].a)) fa2.tran_add_from(fa1, fa1[i]);
+    }
 }
 
 // точка входа в алгоритм
--- grom/grom/11-faau.cpp
+++ grom/grom/11-faau.cpp
@@ -6,25 +6,55 @@
 // a11 -faau
 
 // определяет наличие перехода из финального состояния
-int has_tran_from_final(SYMB A, NFA & fa) {
-    return 0;
+int has_tran_from_final(SYMB A, NFA& fa) {
+	int dc = fa.delta_count();
+	for (int i = 1; i <= dc; i++) {
+		if (fa[i].a == A) return 1;
+	}
+	return 0;
 }
 
 // преобразует автомат в грамматику
-void fa_to_grau(NFA & fa, grammar & gr) {
+void fa_to_grau(NFA& fa, grammar& gr) {
+	gr.set_start(gr.reg_from(fa, fa.initials[1]));
+	RULE rule;
+	TRAN tran;
+	int dc = fa.delta_count();
+	for (int i = 1; i <= dc; i++) {
+		rule.clear();
+		tran = fa[i];
+		rule[0] = gr.reg_from(fa, tran.a);
+		rule.append(gr.reg_char(tran.c));
+		if (fa.finals.contains(tran.b)) {
+			gr.rule_add(rule);
+			if (has_tran_from_final(tran.b, fa)) {
+				rule.append(gr.reg_from(fa, tran.b));
+				gr.rule_add(rule);
+			}
+		}
+		else {
+			rule.append(gr.reg_from(fa, tran.b));
+			gr.rule_add(rule);
+		}
+		/*if (!fa.finals.contains(tran.b) || has_tran_from_final(tran.b, fa)) {
+			gr.rule_add(rule);
+			rule.append(gr.reg_from(fa, tran.b));
+		}
+		gr.rule_add(rule);*/
+	}
 }
 
 // точка входа в алгоритм
 // преобразование автомата в грамматику
-int algorithm_fa_to_grau(NFA & fa, grammar & gr, FILE * target) {
-    // алгоритм
-    fa_to_grau(fa, gr);
-    // выводим грамматику в консоль
-    gr.print(stdout);
-    // выводим грамматику в файл
-    gr.print(target);
-    // результат
-    return 1;
+int algorithm_fa_to_grau(NFA& fa, grammar& gr, FILE* target) {
+	// алгоритм
+	fa_to_grau(fa, gr);
+	// выводим грамматику в консоль
+	gr.print(stdout);
+	// выводим грамматику в файл
+	gr.print(target);
+	// результат
+	return 1;
 }
 /*/
 .a11.sxg
