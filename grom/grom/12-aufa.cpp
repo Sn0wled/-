31e8e911b27ab3b97ee86767028f8db720ff6491
Sn0wled <misha.safin.sn0wled@gmail.com>
2024-02-05 12:57:49 +0500

    aswd

--- grom/grom/12-aufa.cpp
+++ grom/grom/12-aufa.cpp
@@ -8,31 +8,57 @@
 // находит пару A->a или A->aB, возвращает номер правила
 int has_pair_rule(RULE rule, int length, grammar gr) {
     int c = gr.count();
+    RULE r;
     for (int i = 1; i <= c; i++) {
-        RULE r = gr[i];
-        if (r[0] == rule[0] && r[1] == rule[1] && r.count() == length) return i;
+        r = gr[i];
+        if (r[0] == rule[0] && r[1] == rule[1] && r.count() != length) {
+            return i;
+        }
     }
     return 0;
 }
 
 // преобразует грамматику в автомат
 int grau_to_fa(grammar & gr, NFA & fa) {
-    fa.reg_initial_from(gr, gr.start());
     int c = gr.count();
+    RULE rule;
+    int length;
+    TRAN tran;
+    SYMB A, B;
+    char a;
+    fa.reg_initial_from(gr, gr.start());
+
     for (int i = 1; i <= c; i++) {
-        RULE r = gr[i];
-        int rc = r.count();
-        if (rc != 1 && rc != 2) {
-            printf("Grammar is wrong\n");
+        rule = gr[i];
+        length = rule.count();
+        if (length == 2) {
+            if (has_pair_rule(rule, length, gr)) {
+                A = fa.reg_state_from(gr, rule[0]);
+                a = gr.get_char(rule[1]);
+                B = fa.reg_final_from(gr, rule[2]);
+            }
+            else {
+                A = fa.reg_state_from(gr, rule[0]);
+                a = gr.get_char(rule[1]);
+                B = fa.reg_state_from(gr, rule[2]);
+            }
+        }
+        else if (length == 1) {
+            if (!has_pair_rule(rule, length, gr)) {
+                A = fa.reg_state_from(gr, rule[0]);
+                a = gr.get_char(rule[1]);
+                B = fa.get_new_state();
+                fa.reg_final_id(fa.getid(B));
+            }
+        }
+        else {
+            printf("Wrong grammar\n");
             return 0;
         }
-        int length = r.count();
-        int pair = has_pair_rule(r, (1 + length % 2), gr);
-        if (length == 1 && pair != 0) {
-            TRAN t;
-            t.a = fa.reg_state_from(gr, r[0]);
-            t.b
-        }
+        tran.a = A;
+        tran.b = B;
+        tran.c = a;
+        fa.tran_add(tran);
     }
     return 1;
 }
