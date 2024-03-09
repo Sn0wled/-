8b432d170ac74cadce29a3fc6bd7e6badfda1c96
Sn0wled <misha.safin.sn0wled@gmail.com>
2024-03-09 17:13:48 +0500

    доделал лабы за 4 семак

--- grom/grom/16-rexfa.cpp
+++ grom/grom/16-rexfa.cpp
@@ -16,6 +16,7 @@ char current() {
     return rx[index];
 }
 // следующий символ выражени€
+// rx[index++]
 char getnext() {
     char c = rx[index];
     if (c != 10) index++;
@@ -23,58 +24,138 @@ char getnext() {
 }
 // первичный автомат
 void primary(NFA & fa, NFA & part, char c) {
+    TRAN tran;
+    SYMB A = part.reg_state_from(fa, fa.get_new_state());
+    SYMB B = part.reg_state_from(fa, fa.get_new_state());
+    tran.a = A;
+    tran.b = B;
+    tran.c = c;
+    part.tran_add(tran);
+    part.initials.insert(A);
+    part.finals.insert(B);
 }
 // итераци€
 void iteration(NFA & fa, NFA & part) {
+    TRAN tran;
+    SYMB A = part.reg_state_from(fa, fa.get_new_state());
+    SYMB B = part.reg_state_from(fa, fa.get_new_state());
+    tran.c = 0;
+    tran.a = A;
+    tran.b = part.initials[1];
+    part.tran_add(tran);
+    tran.b = B;
+    part.tran_add(tran);
+    tran.a = part.finals[1];
+    part.tran_add(tran);
+    tran.b = part.initials[1];
+    part.tran_add(tran);
+    part.initials.clear();
+    part.finals.clear();
+    part.initials.insert(A);
+    part.finals.insert(B);
 }
 // конкатенаци€
 void concat(NFA & fa, NFA & part, NFA & tail) {
+    TRAN tran;
+    tran.c = 0;
+    tran.a = part.finals[1];
+    tran.b = part.reg_state_from(tail, tail.initials[1]);
+    part.tran_add(tran);
+    part.finals.clear();
+    part.finals.insert(part.reg_final_from(tail, tail.finals[1]));
+    for (int i = 1; i <= tail.delta_count(); i++) {
+        part.tran_add_from(tail, tail[i]);
+    }
 }
 // объединение
 void unite(NFA & fa, NFA & part, NFA & tail) {
+    SYMB A = part.reg_state_from(fa, fa.get_new_state());
+    SYMB B = part.reg_state_from(fa, fa.get_new_state());
+    TRAN t;
+    t.a = A;
+    t.c = 0;
+    t.b = part.initials[1];
+    part.tran_add(t);
+    t.b = part.reg_state_from(tail, tail.initials[1]);
+    part.tran_add(t);
+    t.b = B;
+    t.a = part.finals[1];
+    part.tran_add(t);
+    t.a = part.reg_state_from(tail, tail.finals[1]);
+    part.tran_add(t);
+    for (int i = 1; i <= tail.delta_count(); i++) {
+        part.tran_add_from(tail, tail[i]);
+    }
+    part.initials.clear();
+    part.finals.clear();
+    part.initials.insert(A);
+    part.finals.insert(B);
 }
 // элемент выражени€
 int P(NFA & fa, NFA & part) {
+    int result = 0;
     char c = getnext();
-    int result;
-    if (c == 10) {
-        return 10;
-    }
+    if (c == 10) return 10;
     else if (c == '(') {
-        // выражение в скобках
         result = E(fa, part);
-        if (result != 1) return result;
+        c = getnext();
+        if (c != ')') result = 0;
     }
     else if (c == '1') {
-        // константа
+        primary(fa, part, 0);
+        result = 1;
     }
     else if (c > 122) {
-        // недопустимый знак
+        result = 0;
     }
     else if (c > 96) {
-        // переменна€
+        primary(fa, part, c);
+        result = 1;
     }
-    return 0;
+    return result;
 }
 // унарна€ операци€
 int U(NFA & fa, NFA & part) {
     char c = 0;
-    int result = T(fa, part);
-    return 1;
+    int result = P(fa, part);
+    if (current() == STAR) {
+        c = getnext();
+        iteration(fa, part);
+        while (current() == STAR)
+        {
+            c = getnext();
+        }
+    }
+    return result;
 }
 // конкатенации
 int T(NFA & fa, NFA & part) {
     NFA tail;
     char c = 0;
-    int result = T(fa, part);
-    return 1;
+    int result = U(fa, part);
+    while (current() > 96 && current() < 123 || current() == '(')
+    {
+        tail.clear();
+        result = U(fa, tail);
+        if (result == 0) break;
+        concat(fa, part, tail);
+    }
+    return result;
 }
 // объединени€
 int E(NFA & fa, NFA & part) {
     NFA tail;
     char c = 0;
     int result = T(fa, part);
-    return 1;
+    while (current() == '+')
+    {
+        c = getnext();
+        tail.clear();
+        result = T(fa, tail);
+        if (result != 1) break;
+        unite(fa, part, tail);
+    }
+    return result;
 }
 
 // преобразует регул€рное выражение в конечный автомат
